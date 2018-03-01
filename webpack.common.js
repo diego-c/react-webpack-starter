220398dd16943d4aa10c0dc21153eb4cb86e3040
diego-c <diechaves@gmail.com>
2018-02-28 22:10:07 -0300

    fix filename output

--- webpack.common.js
+++ webpack.common.js
@@ -25,7 +25,7 @@ module.exports = {
                 use: {
                     loader: 'file-loader',
                     options: {
-                        name: './assets/[path][name].[ext]'
+                        name: './assets/[name]_[hash].[ext]'
                     }
                 }
             }                        
