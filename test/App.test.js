d7ff138840d3a1305fff89f66639ef571f0911af
diego-c <diechaves@gmail.com>
2018-03-02 12:26:24 -0300

    fix App test

--- test/App.test.js
+++ test/App.test.js
@@ -1,26 +1,22 @@
-import 'babel-polyfill';
-import 'regenerator-runtime';
 import './adapter.config';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 
 import App from '../src/components/App/App';
 
 describe('<App />', () => {
-    it('successfully renders <Image />', () => {
+    it('successfully renders <Image /> asynchronously', () => {
 
         import('../src/components/Image/Image')
         .then(module => module.default)
         .then(Image => {
+
+            const imgWrapper = shallow(<Image />);
+            imgWrapper.setProps({ path: 'react-logo.png' });
             const wrapper = shallow(<App />);
-            expect(wrapper.find(Image)).toHaveLength(1);
+            expect(wrapper.childAt(0).prop('path')).toEqual(imgWrapper.find('img').prop('src'));
         })
         .catch(err => console.log('Could not load Image: ', err));        
-    });
-
-    it('renders title', () => {
-        const wrapper = shallow(<App />);
-        expect(wrapper.find('h1')).toHaveLength(1);
     });
 });
 
