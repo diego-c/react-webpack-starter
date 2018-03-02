4217f8a7c762c63c7754026e8465880c43f5c3cf
diego-c <diechaves@gmail.com>
2018-03-02 00:13:26 -0300

    try to set up integration with jest and enzyme

---
+++ test/App.test.js
@@ -0,0 +1,14 @@
+import './adapter.config';
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import App from '../src/components/App/App';
+import Image from '../src/components/Image/Image';
+
+describe('<App />', () => {
+    it('successfully renders <Image />', () => {
+        const wrapper = shallow(<App />);
+        expect(wrapper.find(Image)).toHaveLength(1);
+    });
+});
+
