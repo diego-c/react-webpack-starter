adda331a3bec2593450e0af786f585127dcede1c
diego-c <diechaves@gmail.com>
2018-03-05 13:23:03 -0300

    downgrade image-webpack-loader to make it work with firefox

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -37,13 +37,24 @@ module.exports = merge(common, {
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            bypassOnDebug: true,
                             mozjpeg: {
-                                progressive: true,
-                                quality: 65
+                              progressive: true,
+                              quality: 65
                             },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: '65-90',
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            // the webp option will enable WEBP
                             webp: {
-                                quality: 70 
+                              quality: 75
                             }
                         }
                     }
